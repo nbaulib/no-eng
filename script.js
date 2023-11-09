22ebcc8e7a382a89f9c414f4d2d3c085d4363e50
Nico <69161557+nbaulib@users.noreply.github.com>
2023-11-09 18:19:24 -0500

    timer

--- script.js
+++ script.js
@@ -28,3 +28,27 @@ toggleButtons.forEach((button, index) => {
         }
     });
 });
+
+
+function countdownTimer() {
+    const difference = +new Date("2023-11-14T10:00:00") - +new Date();
+    let remaining = "AIRING NOW";
+
+    if (difference > 0) {
+        const parts = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / (1000 * 60)) % 60),
+            seconds: Math.floor((difference / 1000) % 60),
+        };
+
+        remaining = Object.keys(parts).map(part => {
+            return `${parts[part]} ${part}`;
+        }).join(" ");
+    }
+
+    document.getElementById("countdown").innerHTML = remaining;
+}
+
+countdownTimer();
+setInterval(countdownTimer, 1000);
