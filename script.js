b728dc3c3d1d3d0ebc008be5317cdcda3ddcdc6a
Nico <69161557+nbaulib@users.noreply.github.com>
2023-11-12 20:58:40 -0500

    links

--- script.js
+++ script.js
@@ -2,20 +2,6 @@
 const toggleButtons = document.querySelectorAll(".toggleButton");
 const contents = document.querySelectorAll(".content");
 
-//Add click event listeners to each button
-// toggleButtons.forEach((button, index) => {
-//     button.addEventListener("click", function() {
-//         // Toggle the content visibility with each click
-//         if (contents[index].style.display === "block" || contents[index].style.display === "") {
-//             // If it's visible, hide it
-//             contents[index].style.display = "none";
-//         } else {
-//             // If it's hidden or not set, show it
-//             contents[index].style.display = "block";
-//         }
-//     });
-// });
-
 toggleButtons.forEach((button, index) => {
     button.addEventListener("click", function() {
         // Toggle the content visibility with each click
